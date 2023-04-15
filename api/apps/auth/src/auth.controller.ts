5b13352cbf8f08b38b885bfd9f7a3c099c1a42e5
David Ngaba <ngabadavid.nd@gmail.com>
2023-04-15 08:36:56 -0400

    containerization

--- api/apps/auth/src/auth.controller.ts
+++ api/apps/auth/src/auth.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @MessagePattern({ cmd: 'get-users' })
+  async getUsers(@Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    channel.ack(originalMessage);
+
+    return this.authService.getUsers();
+  }
+
+  @MessagePattern({ cmd: 'post-user' })
+  async posttUser(@Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    channel.ack(originalMessage);
+
+    return this.authService.postUser();
   }
 }
